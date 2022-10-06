c4f5ccdce68a1c2a16fb84518a2a190c42bfc76c
lorezvn <94021043+lorezvn@users.noreply.github.com>
2022-10-06 19:10:15 +0200

    Delete constants.py

--- constants.py
+++
@@ -1,5 +0,0 @@
-word_diz = {'names': ['lorenzo', 'francesco', 'alessandro', 'marco', 'eleonora', 'michele', 'giulia'],
-            'cities': ['londra', 'parigi', 'budapest', 'sofia', 'marsiglia', 'nizza', 'roma', 'cagliari', 'verbania', 'vercelli'],
-            'things': ['aspirapolvere', 'astronave', 'automobile', 'asta', 'giavellotto', 'motorino', 'gioco'],
-            'animals':['elefante', 'tigre', 'cane', 'gatto', 'giraffa', 'rinoceronte', 'ippopotamo'],
-            'colors': ['rosso', 'giallo', 'verde', 'blu', 'arancione', 'viola', 'fucsia', 'ciano', 'rosa', 'marrone', 'nero', 'bianco']}
