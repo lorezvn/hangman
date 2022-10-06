b316ea535e95a300eaf4f988b6ce7d52e556c190
lorezvn <94021043+lorezvn@users.noreply.github.com>
2022-10-06 19:10:35 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,5 @@
+word_diz = {'names': ['lorenzo', 'francesco', 'alessandro', 'marco', 'eleonora', 'michele', 'giulia', 'jacopo', 'mattia', 'emanuele', 'giuseppe', 'daniele', 'giovanna'],
+            'cities': ['londra', 'parigi', 'budapest', 'sofia', 'marsiglia', 'nizza', 'roma', 'cagliari', 'verbania', 'vercelli', 'torino', 'rimini', 'bologna', 'kiev', 'vienna', 'varsavia', 'salisburgo', 'dublino'],
+            'things': ['aspirapolvere', 'astronave', 'automobile', 'asta', 'giavellotto', 'motorino', 'gioco', 'dado', 'pallone', 'gomma', 'tagliere', 'tavolo', 'guanto', 'ventilatore', 'lavatrice', 'forno', 'microonde'],
+            'animals':['elefante', 'tigre', 'cane', 'gatto', 'giraffa', 'rinoceronte', 'ippopotamo', 'coccodrillo', 'leone', 'istrice', 'scoiattolo', 'castoro', 'pipistrello', 'alce', 'capriolo'],
+            'colors': ['rosso', 'giallo', 'verde', 'blu', 'arancione', 'viola', 'fucsia', 'ciano', 'rosa', 'marrone', 'nero', 'bianco', 'celeste', 'indaco']}
